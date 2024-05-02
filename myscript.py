51a869dea2226a83a6975e24095c732a24c99987
j-ackamir <ja_amir@live.concordia.ca>
2024-05-02 14:29:16 -0400

    Insert Image Data

--- myscript.py
+++ myscript.py
@@ -1,5 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 from PIL import Image
-pic=Image.open
+
+
+pic = Image.open('DATA/00-puppy.jpg')
+pic_arr = np.asarray(pic)
+
+plt.imshow(pic_arr)
+plt.title('Original Image')
+plt.show()
+
+
+pic_red = pic_arr.copy()
+pic_red[:, :, 1:] = 0
+
+
+plt.imshow(pic_red)
+plt.title('Image with Only Red Channel')
+plt.show()
+
+
+
+
